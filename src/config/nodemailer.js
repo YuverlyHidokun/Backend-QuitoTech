307d536096dc9492a805e606bf38908d174f3d6e
Eduardo-Caza <ecaanaranjo@gmail.com>
2024-12-03 01:06:51 -0500

    arreglo node 2

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -73,7 +73,7 @@ const sendMailToAdmin = (userMail, tienda, token) => {
                 <li><strong>Dirección:</strong> ${Direccion}</li>
                 <li><strong>Email del propietario:</strong> ${userMail}</li>
             </ul>
-            <p>Haz clic <a href="href="http://localhost:3000/propietario/confirmartienda/${encodeURIComponent(token)}">aquí</a> para verificar la tienda.</p>
+            <p>Haz clic <a href="${process.env.URL_FRONTEND}propietario/confirmartienda/${encodeURIComponent(token)}">aquí</a> para verificar la tienda.</p>
             <strong><p>----------------------------------------------------------------------------------------------------</p></strong>
             <p style="color: grey;">Este es un correo electrónico generado por el sistema. No responda a este correo electrónico.</p>
         `
